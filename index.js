7a3dab5035da1d36379267d18a946913f270f4ad
niigyanchristian <niigyanchristian@gmail.com>
2024-07-28 03:16:23 +0000

    version 0.0.1

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/customer", proxy("http://localhost:8001"));
-app.use("/shopping", proxy("http://localhost:8003"));
-app.use("/deliveries", proxy("http://localhost:8004"));
-app.use("/", proxy("http://localhost:8002")); // products
+app.use(process.env.USER_SERVICE_ENDPOINT, proxy(process.env.USER_SERVICE_URL));
+app.use(process.env.SHOPPING_SERVICE_ENDPOINT, proxy(proxyprocess.env.SHOPPING_SERVICE_URL));
+app.use(process.env.DELIVERY_SERVICE_ENDPOINT, proxy(process.env.DELIVERY_SERVICE_URL));
+app.use(process.env.PRODUCT_SERVICE_ENDPOINT, proxy(process.env.PRODUCT_SERVICE_URL)); // products
 
 app.listen(8000, () => {
   console.log("Gateway is Listening to Port 8000");
